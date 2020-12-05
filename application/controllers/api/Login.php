798f71aa89927e95908a93144401e6e5d97cefac
Miikka Moilanen <miikkamoilanen@gmail.com>
2020-12-05 17:14:16 +0200

    test

--- application/controllers/api/Login.php
+++ application/controllers/api/Login.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 
 class Login extends REST_Controller {
+
+  function __construct()
+  {
+      //enable cors
+      header('Access-Control-Allow-Origin: *');
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+      // Construct the parent class
+      parent::__construct();
+  }
+  
     public function Login_post(){
         $this->load->model('Login_model');
         $idCard=$this->post('idCard');
